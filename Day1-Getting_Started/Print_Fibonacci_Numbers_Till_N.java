c3892c8f93ee54e381b21a169c038e0fe368063f
Manish Singh <manish.bk07@gmail.com>
2020-04-27 23:58:08 +0530

    Day 1 Basic Programs

---
+++ Day1-Getting_Started/Print_Fibonacci_Numbers_Till_N.java
@@ -0,0 +1,22 @@
+/**
+ * Print_Fibonacci_Numbers_Till_N
+ */
+import java.util.*;
+public class Print_Fibonacci_Numbers_Till_N {
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    
+    int a = 0;
+    int b = 1;
+    for(int i = 0; i < n; i++){
+        System.out.println(a);
+         int c = a + b;
+         a = b;
+         b = c;
+    }
+    scn.close();
+ }
+
+  
+}
